59f225db8b70e52487299bf9b7b6e9df0e5ac8ff
Marcus Thelin <marcus.thelin@24hr.se>
2020-11-10 23:28:55 +0100

    improved merge request regex

--- src/helpers/__tests__/extract-data-from-url.js
+++ src/helpers/__tests__/extract-data-from-url.js
@@ -2,7 +2,7 @@ const getDataFromUrl = require('../extract-data-from-url');
 describe('Merge request data handling', () => {
     it('Can extract data from merge request url', async () => {
         const extractedData = getDataFromUrl(
-            'https://gitlab.24hr.se/resurs/site/-/merge_requests/913'
+            'https://gitlab.24hr.se/resurs/site/-/merge_requests/913/rest1/rest2'
         );
         expect(extractedData).toBeInstanceOf(Object);
         expect(extractedData).toHaveProperty('projectFullPath');
@@ -11,5 +11,6 @@ describe('Merge request data handling', () => {
         expect(extractedData.projectFullPath).toBe('resurs/site');
         expect(extractedData.type).toBe('merge_requests');
         expect(extractedData.id).toBe('913');
+        expect(extractedData.rest).toBe('rest1/rest2');
     });
 });
